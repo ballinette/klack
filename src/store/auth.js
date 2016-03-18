f24575f2eb0e806f48a41ce8409cc5fd36a20673
ballinette <gballini@kaliop.com>
2016-03-18 06:22:11 +0100

    pre-step12 : use session storage to store JWT Token

--- src/store/auth.js
+++ src/store/auth.js
@@ -8,8 +8,20 @@ export default {
   },
   login (username, password) {
     /** TODO - Step 12: Log in the user to Kuzzle */
+    this.state.errorLogin = null;
+
+    /** Once kuzzle response is received, following code should be called:
+     window.sessionStorage.setItem('jwt', <something>);
+
+     userStore.getCurrentUser(() => {
+        router.go({name: 'home'});
+      });
+     */
   },
   logout () {
     /** TODO - Step 12: Log out the user to Kuzzle */
+
+    userStore.removeCurrentUser();
+    window.sessionStorage.removeItem('jwt');
   }
 }
