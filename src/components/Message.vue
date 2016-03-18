0edb901a0dc2e8aed556048fa24c7ec611343e86
David Bengsch <dbengsch@kaliop.com>
2016-03-18 06:27:07 +0100

    Step 13 : add a condition for message deletion

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -7,8 +7,7 @@
 <template>
   <div class="message">
     <div class="action-hover-container" v-if="deletable">
-      <!-- TODO - Step 13: You mostelikely want to add a condition here; hint : v-if is your friend -->
-      <a class="delete-message" @click="deleteMessage">x</a>
+      <a class="delete-message" @click="deleteMessage" v-if="currentUserId === message.user.id">x</a>
     </div>
     <avatar
       :clickable="false"
